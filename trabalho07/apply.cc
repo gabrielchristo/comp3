5c9668f6cf3da52a3f63c2abd68c9c71f7a7dab8
Gabriel Christo <christo@dcc.ufrj.br>
2022-01-16 19:04:41 -0300

    trabalho 7 90/100

---
+++ trabalho07/apply.cc
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+// type C is container
+// type T is function
+template<typename C, typename T>
+auto apply(const C& list, T F)
+{
+	// vector with same type that function F return
+	vector<decltype(F(*begin(list)))> v;
+	
+	// vector with F result for each item
+	for(const auto& item : list)
+		v.push_back(F(item));
+
+	return v;
+}
