55cd476970be727014999afb0d7c5882ce97889a
Gabriel Christo <christo@dcc.ufrj.br>
2022-02-06 16:13:53 -0300

    trabalho 9 25/100

--- trabalho09/filter.cc
+++ trabalho09/filter.cc
@@ -9,33 +9,31 @@ template<typename C, typename T>
 auto operator|(vector<C> list, T F)
 {
 	auto first = *begin(list);
-	auto returnType = F(first);
-
-	vector<decltype(first)> retElement;
-	vector<decltype(returnType)> retType;
+	vector<decltype(first)> ret;
 
 	// boolean case
-	if constexpr (is_same<decltype(returnType), bool>::value)
+	if constexpr (is_same<decltype(F(first)), bool>::value)
 	{
 		for(const auto& item : list)
-			if(F(item)) retElement.push_back(item);
-		return retElement;
+			if(F(item)) ret.push_back(item);
+		return ret;
 	}
 
 	// void return case
-	else if constexpr (is_same<decltype(returnType), void>::value)
+	else if constexpr (is_same<decltype(F(first)), void>::value)
 	{
 		for(const auto& item : list)
 			F(item);
 	}
 
 	// any other case
-	else
-	{
-        for(const auto &item: list)
-            retType.push_back(F(item));
-        return retType;
-	}
+	//else
+	//{
+	//	vector<decltype(F(first))> retType;
+    //    for(const auto &item: list)
+    //        retType.push_back(F(item));
+    //    return retType;
+	//}
 	
 }
 
@@ -45,7 +43,12 @@ auto operator|(vector<C> list, T F)
 
 int main()
 {
+	// Teste 1
+	//vector<int> v1 = { 2, 9, 8, 8, 7, 4 };
+	//auto result = v1 | []( int x ) { return x % 2 == 0; };
+	//for( auto x : result ) cout << x << " ";
+
+	// Teste 2
 	vector<int> v1 = { 2, 9, 8, 8, 7, 4 };
-	auto result = v1 | []( int x ) { return x % 2 == 0; };
-	for( auto x : result ) cout << x << " ";
+	v1 | []( int x ) { return x % 2 == 0; } | [] ( int x ) { cout << x << " "; };
 }
