d00fa87114cc9f323b3a6ca9b8b7f5dbf51972b4
Gabriel Christo <christo@dcc.ufrj.br>
2022-02-15 02:51:27 -0300

    trabalho 13 36/100

---
+++ trabalho13/bind.cc
@@ -0,0 +1,34 @@
+#include <functional>
+#include <type_traits>
+
+using namespace std;
+
+template<typename F, typename ...Args>
+class Bind
+{
+public:
+
+	Bind(F functor, Args... args) : mFunctor(functor){}
+
+	template<typename ...NewArgs>
+	auto operator()(Args... args, NewArgs... newArgs)
+	{
+		return bind(mFunctor, args..., newArgs...);
+	}
+
+private:
+	F mFunctor;
+};
+
+
+template<typename F, typename ...Args>
+auto bind(F functor, Args... args)
+{
+	if constexpr (is_invocable<decltype(functor), decltype(args)...>::value)
+		return invoke(functor, args...);
+	else
+		return [functor, args...](auto... newArgs)
+		{
+			return invoke(functor, args..., newArgs...);
+		};
+}
